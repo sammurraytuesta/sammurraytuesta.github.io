1e47330b814028ff706008c5d12479596c96f359
samantha murray <60449164+sammurraytuesta@users.noreply.github.com>
2021-01-30 18:10:48 -0800

    removed project description and javaScript

--- js/main.js
+++ js/main.js
@@ -1,101 +1,104 @@
-/* hide nav when scroll */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-}
 
-/* typewriter courtesy of css-tricks.com */
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
+$('document').ready(function () {
+  /* hide nav when scroll */
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
     } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+      document.getElementById("navbar").style.top = "-100px";
     }
+    prevScrollpos = currentScrollPos;
+  }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  /* typewriter courtesy of css-tricks.com */
+  var TxtType = function(el, toRotate, period) {
+      this.toRotate = toRotate;
+      this.el = el;
+      this.loopNum = 0;
+      this.period = parseInt(period, 10) || 2000;
+      this.txt = '';
+      this.tick();
+      this.isDeleting = false;
+  };
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
+  TxtType.prototype.tick = function() {
+      var i = this.loopNum % this.toRotate.length;
+      var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) { delta /= 2; }
+      if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
 
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
+      this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-    setTimeout(function() {
-    that.tick();
-    }, delta);
-};
+      var that = this;
+      var delta = 200 - Math.random() * 100;
 
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #3D2F3D}";
-    document.body.appendChild(css);
-};
+      if (this.isDeleting) { delta /= 2; }
 
-/* form validation */
-const form = document.querySelector('.contact-items');
+      if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+      }
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
+      setTimeout(function() {
+      that.tick();
+      }, delta);
+  };
 
-    
-    const name = form.name.value;
-    const email = form.email.value;
-    const message = form.message.value;
+  window.onload = function() {
+      var elements = document.getElementsByClassName('typewrite');
+      for (var i=0; i<elements.length; i++) {
+          var toRotate = elements[i].getAttribute('data-type');
+          var period = elements[i].getAttribute('data-period');
+          if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+          }
+      }
+      // INJECT CSS
+      var css = document.createElement("style");
+      css.type = "text/css";
+      css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #3D2F3D}";
+      document.body.appendChild(css);
+  };
 
-    if (name == '') {
+  /* form validation */
+  const form = document.querySelector('.contact-items');
 
-        alert("Enter a Valid Name!");
-        form.name.style.borderColor = "red";
-        return false;
-    }
-    if  (email == '') {
-        alert("Enter a Valid Phone Address!");
-        form.email.style.borderColor = "red";
-        return false;
-    }
-    if (message == '') {
-        alert("Please Leave a Message!");
-        form.message.style.borderColor  = "red";
-        return false;
-    }
+  form.addEventListener('submit', e => {
+      e.preventDefault();
 
-    form.submit();
+      
+      const name = form.name.value;
+      const email = form.email.value;
+      const message = form.message.value;
 
+      if (name == '') {
+
+          alert("Enter a Valid Name!");
+          form.name.style.borderColor = "red";
+          return false;
+      }
+      if  (email == '') {
+          alert("Enter a Valid Phone Address!");
+          form.email.style.borderColor = "red";
+          return false;
+      }
+      if (message == '') {
+          alert("Please Leave a Message!");
+          form.message.style.borderColor  = "red";
+          return false;
+      }
+
+      form.submit();
+
+  });
 });
